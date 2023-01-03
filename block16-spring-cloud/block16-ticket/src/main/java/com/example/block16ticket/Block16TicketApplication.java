25e75d0c1946bf55a4481fb243c59579034dc242
Pablo Fuentes <pablo.fuentes@bosonit.com>
2023-01-03 18:18:34 +0100

    block16-Cloud

---
+++ block16-spring-cloud/block16-ticket/src/main/java/com/example/block16ticket/Block16TicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.block16ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block16TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block16TicketApplication.class, args);
+	}
+
+}
