b97d3a7729a0a2233126a7c0e72f01ef6f546c2d
Pablo Fuentes <pablo.fuentes@bosonit.com>
2023-01-12 16:54:11 +0100

    block17-Spring-Batch

--- block1-expression-evaluator/src/main/java/Main.java
+++ block1-expression-evaluator/src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,90 +8,71 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        read_file();
-    }
-
-    private static void revisarCodigo (String infoPersonas) {
-        long count = infoPersonas.chars().filter(character -> character == ':').count();
-
-        if(count == 2){ //Te enseña el primer erroneo;
-            String [] datos = infoPersonas.split(":");
-
-            if (datos[0].isBlank() || datos[0].isEmpty()){
-                System.out.println("Faltaria el nombre de uno de los campos");
-            }
-        }
-    }
-
-    public static void filtrarEdad(ArrayList<Person> Personas){
-        List<Person> listPersonFiltrada = Personas.stream()
-                .filter(persona -> persona.getAge() < 25 && persona.getAge() != 0).collect(Collectors.toList());
-
-        List<Person> empiezaPorA = Personas.stream()
-                .filter(person -> !person.getName().startsWith("A")).collect(Collectors.toList());
-
-        Optional<Person> townIsMadrid = Personas.stream()
-                .filter(person -> person.getTown().equals("Madrid")).findFirst();
-
-        Optional<Person> townIsBarcelona = Personas.stream()
-                .filter(person -> person.getTown().equals("Barcelona")).findFirst();
-
-        listPersonFiltrada.forEach(System.out::println);// listPersonFiltrada.forEach(System.out::println);
-
-        for(Person persona: Personas){
-
-            if (persona.getName() == null){
-                persona.setName("Unknow");
-            }
-            if (persona.getTown() == null){
-                persona.setTown("Unknow");
-            }
-
-            //System.out.println(persona.toString() + "este es el que quiero"); // Este los muestra todos
-        }
+        readFile();
 
     }
 
-    private static Person createPerson (String infoPersonas){
-        Person person = new Person();
+    //1º Leer el archivo
 
-        String[] datosPersona = infoPersonas.split(":");
-
-        person.setName(datosPersona[0]);
-        person.setTown(datosPersona[1]);
-        person.setAge ((datosPersona.length < 3) ? 0 : Integer.parseInt(datosPersona[2]));
-
-        ArrayList<Person> arrayPersonas = new ArrayList<>();
-        arrayPersonas.add(person);
-
-        filtrarEdad(arrayPersonas);
-        revisarCodigo(infoPersonas);
-
-        // System.out.println(person.toString());
-        return person;
-    }
-
-
-    public static void read_file() throws FileNotFoundException {
+    public static void readFile() throws FileNotFoundException {
 
         try {
-            Path entrada = Paths.get("C:/Users/pablo.fuentes/Desktop/people.csv.txt");
-            BufferedReader mibuffer = Files.newBufferedReader(entrada); //recoge el texto
-            String line = mibuffer.readLine();//lee el documento
+            FileReader fileReader = new FileReader("C:/Users/pablo.fuentes/Desktop/people.csv.txt");
+            BufferedReader miBuffer = new BufferedReader(fileReader); //lee el documento
 
-            while (line != null) { // hasta que no encuentre un null no para
-
-                String[] personasData = line.split(":");
-                line = mibuffer.readLine();// que me lea cada linea recortada con split
-                createPerson(line);// a esta información le aplicamos el metodo crear persona
+            String infoPersonas = miBuffer.readLine(); //En linea recojo la información que lee
+            while (infoPersonas != null) { // hasta que no encuentre un null no para de leer
+                infoPersonas = miBuffer.readLine();// que me lea cada linea despues de recortar con split
+                if(infoPersonas != null){
+                    createPerson(infoPersonas);// a esta información le aplicamos el metodo crear persona
+                } else{
+                   // System.out.println("Fin del documento");
+                }
             }
-
-        }   catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    //2º Con esos datos creas personas
+
+    private static void createPerson(String infoPersonas) {
+        List<Person> personas = new ArrayList<>();
+        Person person = new Person();
+        try{
+            String[] datosPersona = infoPersonas.split(":");
+            person.setName(datosPersona[0]);
+
+            if (datosPersona.length >= 2) {
+                if (datosPersona[1].isEmpty()) {
+                    person.setTown("Desconocida");
+                } else {
+                    person.setTown(datosPersona[1]);
+                }
+            }
+            person.setAge((datosPersona.length < 3) ? 0 : Integer.parseInt(datosPersona[2]));
+            personas.add(person);
+            filtrarPersonas(personas);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    //3º Con este metodo filtramos los datos
+
+    public static void filtrarPersonas(List<Person> personas){
+        List<Person> personList = personas;
+        personList.stream()
+                .filter(p -> p.getName() != null && p.getTown() != null && p.getAge() < 25 && p.getAge() != 0)
+                .forEach(person -> System.out.println("Nombre: " + person.getName() + ", Ciudad: " + person.getTown() +
+                        ", Edad: " + person.getAge()));
+    }
 }
+
+
